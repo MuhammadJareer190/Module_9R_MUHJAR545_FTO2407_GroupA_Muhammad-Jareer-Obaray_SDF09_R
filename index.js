5de8c1069fddf8bc62f317aec2d9f31eaff21e6b
Muhammad Jareer <jareerobaray@icloud.com>
2024-08-01 14:14:41 +0200

    everthing is working all thank's to Mr Joshua

--- index.js
+++ index.js
@@ -42,17 +42,17 @@ function startGame() {
 function renderGame() {
     cardEl.textContent = "Card: " 
     for (let i = 0; i < cards.length; i++) {
-        cardEl.textContent += cards[i] 
+        cardEl.textContent += cards[i] + " " 
     } 
 
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Woohoo! You've got Blackjack! 🥳"
+        message = "Woohoo! You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
     messageEl.textContent = message
